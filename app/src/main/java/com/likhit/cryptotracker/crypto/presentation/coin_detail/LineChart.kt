053051b9e8e5f3898b78ad2a55b6e720a6868675
Likhit Sakhare <sakhare1181likhit@gmail.com>
2025-04-05 16:51:52 +0530

    Create some utils of coin_detail and currently working on Line chart

---
+++ app/src/main/java/com/likhit/cryptotracker/crypto/presentation/coin_detail/LineChart.kt
@@ -0,0 +1,59 @@
+package com.likhit.cryptotracker.crypto.presentation.coin_detail
+
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.rememberTextMeasurer
+
+@Composable
+fun LineChart(
+    dataPoints: List<DataPoint>,
+    style: ChartStyle,
+    visibleDataPointIndices: IntRange,
+    unit: String,
+    modifier: Modifier = Modifier,
+    showHelperLines: Boolean = true,
+    selectedDataPoint: DataPoint? = null,
+    onSelectDataPoint: (DataPoint) -> Unit = {},
+    onXLabelWidthChange: (Float) -> Unit = {}
+) {
+    val textStyle = LocalTextStyle.current.copy(
+        fontSize = style.labelFontSize
+    )
+
+    val visibleDataPoints = remember(dataPoints, visibleDataPointIndices) {
+        dataPoints.slice(visibleDataPointIndices)
+    }
+
+    val maxYValue = remember(visibleDataPoints) {
+        visibleDataPoints.maxOfOrNull { it.y }?: 0f
+    }
+    val minYValue = remember(visibleDataPoints) {
+        visibleDataPoints.minOfOrNull { it.y }?: 0f
+    }
+
+    val measurer = rememberTextMeasurer()
+
+    var xLabelWidth by remember {
+        mutableFloatStateOf(0f)
+    }
+    LaunchedEffect(xLabelWidth) {
+        onXLabelWidthChange(xLabelWidth)
+    }
+
+    val selectedDataPointIndex = remember(selectedDataPoint) {
+        dataPoints.indexOf(selectedDataPoint)
+    }
+    var drawPoints by remember {
+        mutableStateOf(listOf<DataPoint>())
+    }
+    var isShowingDataPoints by remember {
+        mutableStateOf(selectedDataPoint != null)
+    }
+}
